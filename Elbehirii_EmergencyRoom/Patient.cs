9bc044b0f2f4abb44bebc38b1abea8c583d5f032
Bahaa <Bahaa@DESKTOP-FA8A547>
2023-11-29 15:19:36 -0800

    Add project files.

---
+++ Elbehirii_EmergencyRoom/Patient.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elbehirii_EmergencyRoom
+{
+    internal class Patient
+    {
+
+        private string lastName;
+        private string firstName;
+        private string birthday;
+        private DateOnly DOB;
+        public int priority; // smaller values are higher priority
+
+
+        public Patient(string lastName, string firstName, string birthday, int priority) //, int priority
+        {
+            this.lastName = lastName;
+            this.firstName = firstName;
+            this.birthday = birthday;
+            int[] test = { int.Parse(birthday.Split('/')[2]), int.Parse(birthday.Split('/')[0]), int.Parse(birthday.Split('/')[1]) };
+            //[year, month, day]
+            DOB = DateOnly.FromDateTime(Convert.ToDateTime(birthday));
+            int age = DateOnly.FromDateTime(DateTime.Now).Year - DOB.Year;
+            if (age < 21 || age > 65)
+            {
+                priority += 1;
+            }
+            this.priority = priority;
+
+        }
+        public Patient(int priority)
+        {
+            this.priority = priority;
+        }
+        public override string ToString()
+        {
+            //    return (lastName + firstName + " ," + birthday + priority);// toString LINE
+            return " " + lastName + "," + firstName + "," + birthday + "," + priority.ToString() + " ";
+        }
+        public int CompareTo(Patient other)
+        {
+            if (this.priority < other.priority) return -1;
+            else if (this.priority > other.priority) return 1;
+            else return 0;
+        }
+
+    }
+}
---
+++ Elbehirii_EmergencyRoom/Program.cs
@@ -0,0 +1,117 @@
+﻿namespace Elbehirii_EmergencyRoom
+{
+    internal class Program
+    {
+        public static PriorityQueue<Patient, int> myqueue = new PriorityQueue<Patient, int>();
+        static void Main(string[] args)
+        {
+            string ReaderLastname;
+            string ReaderFirstname;
+            string ReaderBirthday;
+            int Readerpriorty;
+
+
+            StreamReader sr = new StreamReader(Environment.CurrentDirectory + "\\Patients.csv");
+            string? currLine = sr.ReadLine();
+
+            while (currLine != null)
+            {
+                currLine = sr.ReadLine();
+                if (currLine == null) { continue; }
+                Patient newPat = new Patient(currLine.Split(',')[0], currLine.Split(',')[1], currLine.Split(',')[2], int.Parse(currLine.Split(',')[3]));
+                myqueue.Enqueue(newPat, newPat.priority);
+            }
+
+            sr.Close();
+
+            char choice;
+
+
+            do
+            {
+                
+                Console.WriteLine("=== Main Menu ====");
+                Console.WriteLine("===================");
+                Console.WriteLine("(A)dd Patient  (P)rocess Current Patient  (L)ist All in Queue  (Q)uit");
+                Console.WriteLine("===================");
+                Console.Write("    'hot' key as shown above in parenthesis:  ");
+                try
+                {
+                    choice = Convert.ToChar(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.Write(" Choose next choice: ");
+                    choice = Convert.ToChar(Console.ReadLine());
+
+                }
+                switch (choice)
+                {
+
+                    case 'A':
+
+                        Console.Write("Patient Lastname:  ");
+                        ReaderLastname = Convert.ToString(Console.ReadLine());
+                        Console.Write("Patient Firstname:  ");
+                        ReaderFirstname = Convert.ToString(Console.ReadLine());
+                        Console.Write("Patient Birthday:  ");
+                        ReaderBirthday = Convert.ToString(Console.ReadLine());
+
+                        Console.Write("Priority (1:highest priority – 5:lowest priority) ");
+                        Readerpriorty = Convert.ToInt32(Console.ReadLine());
+                        if (Readerpriorty > 5 || Readerpriorty < 1)
+                        {
+                            Console.Write("Priority (1:highest priority – 5:lowest priority)");
+                            Readerpriorty = Convert.ToInt32(Console.ReadLine());
+                        }
+
+                        Patient er = new Patient(ReaderLastname, ReaderFirstname, ReaderBirthday, Readerpriorty);
+                        // priorty1 = Convert.ToInt32(Console.ReadLine());
+
+
+                        Patient e1 = new Patient(ReaderLastname, ReaderFirstname, ReaderBirthday, Readerpriorty);
+                        myqueue.Enqueue(e1, e1.priority);
+
+                        break;
+
+                    case 'P':
+
+                        //if (myqueue.Count > 0)
+                        Console.WriteLine(myqueue.Dequeue());
+
+                        break;
+
+                    case 'L':
+
+                        Disp();
+
+                        break;
+
+                    case 'Q':
+
+                        break;
+
+                }
+
+            } while (choice != 'Q');
+
+        }
+        static public void Disp()
+        {
+            PriorityQueue<Patient, int> tempQueue = new PriorityQueue<Patient, int>();
+            int listCount = myqueue.Count;
+            for (int item = 0; item < listCount; item++)
+            {
+                Patient tempPatient = myqueue.Dequeue();
+                Console.WriteLine(tempPatient.ToString());
+                tempQueue.Enqueue(tempPatient, tempPatient.priority);
+            }
+            listCount = tempQueue.Count;
+            for (int item = 0; item < listCount; item++)
+            {
+                Patient tempPatient = tempQueue.Dequeue();
+                myqueue.Enqueue(tempPatient, tempPatient.priority);
+            }
+        }
+    }
+}
